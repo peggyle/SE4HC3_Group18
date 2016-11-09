3d22e576c407c9cc0b0507ec0d68d812fa6ca09d
peggyle <peggyle.18@gmail.com>
2016-11-08 19:21:44 -0500

    add folder again

---
+++ 4HC3 Assignment 2 - Code/HC3 A2/bin/Debug/CheckAccount3.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HC3_A2
+{
+    /// <summary>
+    /// Interaction logic for Page3.xaml
+    /// </summary>
+    public partial class CheckAccount3 : Page
+    {
+        public CheckAccount3()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_click(object sender, RoutedEventArgs e)
+        {
+            CheckAccount1 logout = new CheckAccount1();
+            this.NavigationService.Navigate(logout);
+        }
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page4());
+        }
+
+    }
+}
