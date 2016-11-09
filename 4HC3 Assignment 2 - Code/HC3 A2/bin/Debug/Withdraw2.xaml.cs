2f0877f65a063a604056beab4b359541056ffbd7
peggyle <peggyle.18@gmail.com>
2016-11-09 10:26:32 -0500

    working on withdrawal

--- 4HC3 Assignment 2 - Code/HC3 A2/bin/Debug/Withdraw2.xaml.cs
+++ 4HC3 Assignment 2 - Code/HC3 A2/bin/Debug/Withdraw2.xaml.cs
@@ -90,7 +90,7 @@ namespace HC3_A2
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            this.NavigationService.Navigate(new HC3_A2.Withdraw3()); 
+            this.NavigationService.Navigate(new HC3_A2.Withdraw1()); 
         }
 
         private void OKButton_Click(object sender, RoutedEventArgs e)
@@ -101,6 +101,22 @@ namespace HC3_A2
         private void TextFold(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void buttonPressed(object sender, RoutedEventArgs e)
+        {
+            Button b = e.Source as Button;
+            b.Effect = null;
+            TranslateTransform trans = new TranslateTransform(3, 3);
+            b.RenderTransform = trans;
+        }
+
+        private void buttonReleased(object sender, RoutedEventArgs e)
+        {
+            Button b = e.Source as Button;
+            b.Effect = new System.Windows.Media.Effects.DropShadowEffect();
+            TranslateTransform trans = new TranslateTransform(-3, -3);
+            b.RenderTransform = trans;
         }
     }
 }
